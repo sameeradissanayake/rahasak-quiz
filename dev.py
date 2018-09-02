000537c18c4036e8de028dd95a627af55b0fcd63
sameera <samees3dissanayake@gmail.com>
2018-09-02 23:17:36 +0530

    first commit

--- dev.py
+++ dev.py
@@ -4,9 +4,11 @@ from kafka import KafkaConsumer
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 consumer = KafkaConsumer('verifiedData', bootstrap_servers='localhost:9092')
 
+#user input
 userInput = raw_input('Enter Number:')
 producer.send('userData', value = str.encode(userInput))
 
+#print ops result
 for message in consumer:
 	print(message.value)
 	break
--- ops.py
+++ ops.py
@@ -5,14 +5,11 @@ from kafka import KafkaProducer
 consumer = KafkaConsumer('userData', bootstrap_servers='localhost:9092')
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-try:
-    for message in consumer:
-    	rule = re.compile(r'(^[+0-9]{1,3})*([0-9]{10,11}$)')
-        if rule.search(message.value):
-        	producer.send('verifiedData', value = b'yes')
-        else:
-        	producer.send('verifiedData', value = b'no')
-
-except:
-	print "Error"
-
+# receive user input
+for message in consumer:
+	rule = re.compile(r'(^[+0-9]{1,3})*([0-9]{10,11}$)')
+	#phone number verification & sending result
+	if rule.search(message.value):
+		producer.send('verifiedData', value = b'yes')
+	else:
+		producer.send('verifiedData', value = b'no')
